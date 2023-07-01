48ed39a0ad78777fad3665b271635fa90b824297
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-01 20:01:11 +0530

    Add files via upload

---
+++ PARLIAMENT.py
@@ -0,0 +1,16 @@
+# cook your dish here
+#CEO EFFECT 
+#KCPD 
+#SKILLER S DAY 17-6-2023
+
+
+import math
+t=int(input())
+for i in range(t):
+    n,x=list(map(int,input().split()))
+    
+    if 2*x>=n:
+        print('yes')
+    else:
+        print('no')
+
