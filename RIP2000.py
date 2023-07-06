7efffb13c3170e427057eda729571c727e4b2db1
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-06 10:46:32 +0530

    Add files via upload

---
+++ RIP2000.py
@@ -0,0 +1,16 @@
+# cook your dish here
+
+#SALAAR TEASER TOMORROW 
+#5:12 AM
+def calculate_notes_needed(n):
+    
+    notes_needed = n * 4
+    return notes_needed
+n = int(input())
+
+result = calculate_notes_needed(n)
+
+
+print(result)
+
+
