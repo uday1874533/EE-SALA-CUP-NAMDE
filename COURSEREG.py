67ee5f48bcff87cb12641d7dc6a8e8d4273025ec
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-06 11:15:58 +0530

    Add files via upload

---
+++ COURSEREG.py
@@ -0,0 +1,10 @@
+# cook your dish here
+# SALAAR CEASEFIRE 
+# ARACHAKAM 
+
+for _ in range(int(input())):
+    n,m,k = map(int,input().split())
+    if m - k >= n :
+        print('yes')
+    else:
+        print('no')
