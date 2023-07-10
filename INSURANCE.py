b4bc43a1b9ce56d2b4b2956dbf9a73b75c7a335c
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-10 17:32:43 +0530

    Add files via upload

---
+++ INSURANCE.py
@@ -0,0 +1,12 @@
+# cook your dish here
+#CSP COMPLTED ENJOYED A LOT HOO HOO
+for _ in range(int(input())):
+    x,y=map(int,input().split(" "))
+    if y<x:
+        print(y)
+    else:
+        print(x)
+        
+        
+        
+
